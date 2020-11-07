1c68ef4ace3c39fae85eb622499725c1d1d1da5f
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-07 17:20:05 +0530

    rendering p tag

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
-  )
+    <p>I am learning React. My life is getting better.</p>
+  );
 }
-
+ReactDOM.render(App, document.getElementById("root"));
 
 export default App;
