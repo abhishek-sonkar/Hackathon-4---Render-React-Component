d8727a14a5cb955b833fc5dde0be951626b7fc7a
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-16 13:04:23 +0530

    corrected code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
-import React, {Component, useState} from "react";
-import ReactDOM from 'react-dom';
+import React from "react";
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    document.createElement('p').innerText = "I am learning React. My life is getting better."
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <p>I am learning React. My life is getting better.</p>
+    );
+  }
 }
-ReactDOM.render(App(), document.getElementById("root"));
-
-export default App;
